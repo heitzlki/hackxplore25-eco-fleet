9e0efeb1ebe4577203fceee1a115659d234eee10
TotallyInformatik <totallyturing@gmail.com>
2025-05-16 22:47:07 +0200

    clown rui pushed merge conflicts what a komiker, now fixed

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
   eslint: {
-    // Disable ESLint during production builds
-=======
-  lint: {
->>>>>>> 93c17d0e5f8d85ae8a4aacb1a506d752814cd2b2
     ignoreDuringBuilds: true,
   },
 };
