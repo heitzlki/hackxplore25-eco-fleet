b230c518675b1e6194778bd14dd08601acce37ab
TotallyInformatik <totallyturing@gmail.com>
2025-05-17 11:05:16 +0200

    mock llm endpoint done

--- client/src/app/api/openai/petzen/route.ts
+++ client/src/app/api/openai/petzen/route.ts
@@ -5,9 +5,10 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
 
   const response = await client.responses.create({
-    model: "gpt-4.1",
+    model: "claude-3-5-sonnet",
     input: "Write a one-sentence bedtime story about a unicorn.",
   });
 
   return NextResponse.json(response);
+
 }
