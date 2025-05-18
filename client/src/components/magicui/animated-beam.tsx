d41b0d7c6f1389ce81e62f356532a3fdf1643fc3
heitzlki <kirill.heitzler@gmail.com>
2025-05-18 09:39:49 +0200

    fix stlyes

--- client/src/components/magicui/animated-beam.tsx
+++ client/src/components/magicui/animated-beam.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { motion } from "motion/react";
-import { RefObject, useEffect, useId, useState } from "react";
+import { motion } from 'motion/react';
+import { RefObject, useEffect, useId, useState } from 'react';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 export interface AnimatedBeamProps {
   className?: string;
@@ -34,33 +34,33 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
   reverse = false, // Include the reverse prop
   duration = Math.random() * 3 + 4,
   delay = 0,
-  pathColor = "gray",
+  pathColor = 'gray',
   pathWidth = 2,
   pathOpacity = 0.2,
-  gradientStartColor = "#ffaa40",
-  gradientStopColor = "#9c40ff",
+  gradientStartColor = '#38e8b6',
+  gradientStopColor = '#4cc9f0',
   startXOffset = 0,
   startYOffset = 0,
   endXOffset = 0,
   endYOffset = 0,
 }) => {
   const id = useId();
-  const [pathD, setPathD] = useState("");
+  const [pathD, setPathD] = useState('');
   const [svgDimensions, setSvgDimensions] = useState({ width: 0, height: 0 });
 
   // Calculate the gradient coordinates based on the reverse prop
   const gradientCoordinates = reverse
     ? {
-        x1: ["90%", "-10%"],
-        x2: ["100%", "0%"],
-        y1: ["0%", "0%"],
-        y2: ["0%", "0%"],
+        x1: ['90%', '-10%'],
+        x2: ['100%', '0%'],
+        y1: ['0%', '0%'],
+        y2: ['0%', '0%'],
       }
     : {
-        x1: ["10%", "110%"],
-        x2: ["0%", "100%"],
-        y1: ["0%", "0%"],
-        y2: ["0%", "0%"],
+        x1: ['10%', '110%'],
+        x2: ['0%', '100%'],
+        y1: ['0%', '0%'],
+        y2: ['0%', '0%'],
       };
 
   useEffect(() => {
@@ -124,40 +124,39 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
 
   return (
     <svg
-      fill="none"
+      fill='none'
       width={svgDimensions.width}
       height={svgDimensions.height}
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns='http://www.w3.org/2000/svg'
       className={cn(
-        "pointer-events-none absolute left-0 top-0 transform-gpu stroke-2",
-        className,
+        'pointer-events-none absolute left-0 top-0 transform-gpu stroke-2',
+        className
       )}
-      viewBox={`0 0 ${svgDimensions.width} ${svgDimensions.height}`}
-    >
+      viewBox={`0 0 ${svgDimensions.width} ${svgDimensions.height}`}>
       <path
         d={pathD}
         stroke={pathColor}
         strokeWidth={pathWidth}
         strokeOpacity={pathOpacity}
-        strokeLinecap="round"
+        strokeLinecap='round'
       />
       <path
         d={pathD}
         strokeWidth={pathWidth}
         stroke={`url(#${id})`}
-        strokeOpacity="1"
-        strokeLinecap="round"
+        strokeOpacity='1'
+        strokeLinecap='round'
       />
       <defs>
         <motion.linearGradient
-          className="transform-gpu"
+          className='transform-gpu'
           id={id}
-          gradientUnits={"userSpaceOnUse"}
+          gradientUnits={'userSpaceOnUse'}
           initial={{
-            x1: "0%",
-            x2: "0%",
-            y1: "0%",
-            y2: "0%",
+            x1: '0%',
+            x2: '0%',
+            y1: '0%',
+            y2: '0%',
           }}
           animate={{
             x1: gradientCoordinates.x1,
@@ -171,16 +170,14 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
             ease: [0.16, 1, 0.3, 1], // https://easings.net/#easeOutExpo
             repeat: Infinity,
             repeatDelay: 0,
-          }}
-        >
-          <stop stopColor={gradientStartColor} stopOpacity="0"></stop>
+          }}>
+          <stop stopColor={gradientStartColor} stopOpacity='0'></stop>
           <stop stopColor={gradientStartColor}></stop>
-          <stop offset="32.5%" stopColor={gradientStopColor}></stop>
+          <stop offset='32.5%' stopColor={gradientStopColor}></stop>
           <stop
-            offset="100%"
+            offset='100%'
             stopColor={gradientStopColor}
-            stopOpacity="0"
-          ></stop>
+            stopOpacity='0'></stop>
         </motion.linearGradient>
       </defs>
     </svg>
--- client/src/components/magicui/typing-animation.tsx
+++ client/src/components/magicui/typing-animation.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { cn } from "@/lib/utils";
-import { motion, MotionProps } from "motion/react";
-import { useEffect, useRef, useState } from "react";
+import { cn } from '@/lib/utils';
+import { motion, MotionProps } from 'motion/react';
+import { useEffect, useRef, useState } from 'react';
 
 interface TypingAnimationProps extends MotionProps {
   children: string;
@@ -18,7 +18,7 @@ export function TypingAnimation({
   className,
   duration = 100,
   delay = 0,
-  as: Component = "div",
+  as: Component = 'div',
   startOnView = false,
   ...props
 }: TypingAnimationProps) {
@@ -26,7 +26,7 @@ export function TypingAnimation({
     forwardMotionProps: true,
   });
 
-  const [displayedText, setDisplayedText] = useState<string>("");
+  const [displayedText, setDisplayedText] = useState<string>('');
   const [started, setStarted] = useState(false);
   const elementRef = useRef<HTMLElement | null>(null);
 
@@ -47,7 +47,7 @@ export function TypingAnimation({
           observer.disconnect();
         }
       },
-      { threshold: 0.1 },
+      { threshold: 0.1 }
     );
 
     if (elementRef.current) {
@@ -79,11 +79,10 @@ export function TypingAnimation({
     <MotionComponent
       ref={elementRef}
       className={cn(
-        "text-4xl font-bold leading-[5rem] tracking-[-0.02em]",
-        className,
+        'text-3xl font-bold leading-[5rem] tracking-[-0.02em]',
+        className
       )}
-      {...props}
-    >
+      {...props}>
       {displayedText}
     </MotionComponent>
   );
